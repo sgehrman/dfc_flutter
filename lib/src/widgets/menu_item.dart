b3e437659ece01dc3e0b0bd8031e392d4e393c57
Steve Gehrman <sgehrman@gmail.com>
2021-08-11 21:43:46 -0700

    fix

--- lib/src/widgets/menu_item.dart
+++ lib/src/widgets/menu_item.dart
@@ -4,23 +4,31 @@ class MenuItem extends StatelessWidget {
   const MenuItem({
     required this.name,
     this.icon,
+    this.level = 0,
   });
 
   final String name;
   final Icon? icon;
+  final int level;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      contentPadding: EdgeInsets.only(left: level * 20),
+      dense: true,
+      horizontalTitleGap: 0,
       // IntrinsicWidth keeps the width of the icon without expanding endlessly
-      leading: IntrinsicWidth(
-        child: Container(
-          alignment: Alignment.centerLeft,
-          child: icon,
-        ),
-      ),
+      leading: icon != null
+          ? IntrinsicWidth(
+              child: Container(
+                alignment: Alignment.centerLeft,
+                child: icon,
+              ),
+            )
+          : null,
       title: Text(
         name,
+        style: const TextStyle(fontSize: 20),
         overflow: TextOverflow.ellipsis,
       ),
     );
