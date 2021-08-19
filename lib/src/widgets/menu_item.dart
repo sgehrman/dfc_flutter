b6c6307dc63dcc2c0a979dee5dff6ce3e0588b27
Steve Gehrman <sgehrman@gmail.com>
2021-08-19 14:59:51 -0700

    fix

--- lib/src/widgets/menu_item.dart
+++ lib/src/widgets/menu_item.dart
@@ -5,17 +5,20 @@ class MenuItem extends StatelessWidget {
     required this.name,
     this.icon,
     this.level = 0,
+    this.onTap, // not needed in a PopupMenuItem
   });
 
   final String name;
   final Icon? icon;
   final int level;
+  final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10.0),
       child: ListTile(
+        onTap: onTap,
         contentPadding: EdgeInsets.only(left: level * 20),
         visualDensity: VisualDensity.compact,
         horizontalTitleGap: 0,
