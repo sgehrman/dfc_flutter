7ec7ae4f694ea108348b0f1f23352039503c14c5
Steve Gehrman <sgehrman@gmail.com>
2021-08-23 16:36:52 -0700

    fix

--- lib/src/svg_icons/svg_screen.dart
+++ lib/src/svg_icons/svg_screen.dart
@@ -1,9 +1,11 @@
 import 'package:dfc_flutter/src/svg_icons/svg_converter.dart';
 import 'package:dfc_flutter/src/svg_icons/svg_icon.dart';
 import 'package:dfc_flutter/src/svg_icons/svg_icons.dart';
+import 'package:dfc_flutter/src/utils/utils.dart';
+import 'package:dfc_flutter/src/widgets/search_field.dart';
 import 'package:flutter/material.dart';
 
-class SvgScreen extends StatelessWidget {
+class SvgScreen extends StatefulWidget {
   const SvgScreen({
     this.color = Colors.grey,
     this.size = 50,
@@ -11,6 +13,13 @@ class SvgScreen extends StatelessWidget {
 
   final Color color;
   final double size;
+
+  @override
+  State<SvgScreen> createState() => _SvgScreenState();
+}
+
+class _SvgScreenState extends State<SvgScreen> {
+  String _filter = '';
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +35,7 @@ class SvgScreen extends StatelessWidget {
       ),
     ];
 
-    final gridItemWidth = size * 3;
+    final gridItemWidth = widget.size * 3;
     final gridItemHeight = gridItemWidth;
 
     return Scaffold(
@@ -34,33 +43,72 @@ class SvgScreen extends StatelessWidget {
         title: const Text('SVG Icons'),
         actions: actions,
       ),
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          return GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: constraints.maxWidth ~/ gridItemWidth,
-              mainAxisExtent: gridItemHeight,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
-            ),
-            itemCount: icons.length,
-            itemBuilder: (context, index) {
-              return Column(
-                children: [
-                  const SizedBox(height: 20),
-                  SvgIcon(
-                    icons[index],
-                    size: size,
-                    color: color,
-                  ),
-                  const SizedBox(height: 10),
-                  Text(iconNames[index]),
-                ],
-              );
+      body: Column(
+        children: [
+          SearchField(
+            onChange: (value) {
+              _filter = value;
+              setState(() {});
             },
-          );
-        },
+            onSubmit: (value) {},
+            hint: 'Filter',
+            label: '',
+          ),
+          Expanded(
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                final List<_NameAndIcon> items = [];
+
+                for (int i = 0; i < iconNames.length; i++) {
+                  if (Utils.isNotEmpty(_filter)) {
+                    if (iconNames[i].contains(_filter)) {
+                      items.add(_NameAndIcon(iconNames[i], icons[i]));
+                    }
+                  } else {
+                    items.add(_NameAndIcon(iconNames[i], icons[i]));
+                  }
+                }
+
+                return GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: constraints.maxWidth ~/ gridItemWidth,
+                    mainAxisExtent: gridItemHeight,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                  ),
+                  itemCount: items.length,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        const SizedBox(height: 20),
+                        SvgIcon(
+                          items[index].icon,
+                          size: widget.size,
+                          color: widget.color,
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          items[index].name,
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
+}
+
+class _NameAndIcon {
+  const _NameAndIcon(
+    this.name,
+    this.icon,
+  );
+
+  final String name;
+  final String icon;
 }
