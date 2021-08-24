08e1dfe6fff0a4deb417ecba096a69eb3d6ae3e4
Steve Gehrman <sgehrman@gmail.com>
2021-08-24 15:05:45 -0700

    icon fix

--- lib/src/svg_icons/svg_icon.dart
+++ lib/src/svg_icons/svg_icon.dart
@@ -14,14 +14,14 @@ class SvgIcon extends StatelessWidget {
     this.size = 24,
     this.fit = BoxFit.contain,
     this.allowDrawingOutsideViewBox = false,
-    this.color = Colors.black,
+    this.color,
     this.mainAxisAlignment = MainAxisAlignment.center,
     this.animationDuration = const Duration(milliseconds: 300),
     this.animationCurve = Curves.fastOutSlowIn,
   });
 
   final double? size;
-  final Color color;
+  final Color? color;
   final BoxFit fit;
   final bool allowDrawingOutsideViewBox;
   final MainAxisAlignment mainAxisAlignment;
@@ -29,10 +29,13 @@ class SvgIcon extends StatelessWidget {
   final Duration animationDuration;
   final Curve animationCurve;
 
-  String _webFix(String svg) {
+  String _webFix(
+    String svg,
+    Color svgColor,
+  ) {
     String svgString = svg;
 
-    String hexString = color.value.toRadixString(16).padLeft(6, '0');
+    String hexString = svgColor.value.toRadixString(16).padLeft(6, '0');
     hexString = hexString.substring(2, hexString.length);
 
     svgString = svgString.replaceAll(
@@ -41,8 +44,8 @@ class SvgIcon extends StatelessWidget {
     );
 
     // fill-opacity doesn't seem to work globally like the fill, so we add it to each path
-    if (color.opacity < 1.0) {
-      String opacity = (color.opacity * 10).toInt().toString();
+    if (svgColor.opacity < 1.0) {
+      String opacity = (svgColor.opacity * 10).toInt().toString();
       opacity = '0.$opacity';
 
       svgString = svgString.replaceAll(
@@ -73,8 +76,15 @@ class SvgIcon extends StatelessWidget {
   Widget build(BuildContext context) {
     String svgString = svg;
 
+    Color svgColor = color ?? Colors.black;
+
+    // if no color passed in, use the current theme color
+    if (color == null) {
+      svgColor = Theme.of(context).iconTheme.color ?? Colors.black;
+    }
+
     if (Utils.isWeb) {
-      svgString = _webFix(svgString);
+      svgString = _webFix(svgString, svgColor);
     }
 
     return Row(
@@ -87,7 +97,7 @@ class SvgIcon extends StatelessWidget {
           curve: animationCurve,
           child: SvgPicture.string(
             svgString,
-            color: color,
+            color: svgColor,
             width: size,
             fit: fit,
             allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
