879faf02b785afd018fffb487650bea53a8118d3
Steve Gehrman <sgehrman@gmail.com>
2021-08-24 20:45:54 -0700

    fix

--- lib/src/themes/platform_sizes.dart
+++ lib/src/themes/platform_sizes.dart
@@ -8,6 +8,7 @@ class Sizes {
     required this.s,
     required this.m,
     required this.l,
+    required this.xl,
   });
 
   // tried to make this const, but doesn't seem provide what I need
@@ -16,22 +17,43 @@ class Sizes {
   factory Sizes.fonts() {
     // smaller fonts for mac desktop
     if (Utils.isMacOS) {
-      return const Sizes(s: 12, m: 14, l: 18);
+      return const Sizes(
+        s: 12,
+        m: 14,
+        l: 18,
+        xl: 30,
+      );
     }
 
-    return const Sizes(s: 14, m: 16, l: 20);
+    return const Sizes(
+      s: 14,
+      m: 16,
+      l: 20,
+      xl: 40, // used for quote
+    );
   }
 
   factory Sizes.icons() {
     // smaller fonts for mac desktop
     if (Utils.isMacOS) {
-      return const Sizes(s: 18, m: 22, l: 32);
+      return const Sizes(
+        s: 18,
+        m: 22,
+        l: 32,
+        xl: 42,
+      );
     }
 
-    return const Sizes(s: 18, m: 22, l: 32);
+    return const Sizes(
+      s: 18,
+      m: 22,
+      l: 32,
+      xl: 42,
+    );
   }
 
   final double s;
   final double m;
   final double l;
+  final double xl;
 }
