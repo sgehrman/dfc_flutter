b10a9f56e65d37bda8888e0386265677a4dc7e27
Steve Gehrman <sgehrman@gmail.com>
2021-09-09 20:18:17 -0700

    fix

--- lib/src/widgets/drag_scroll_widget.dart
+++ lib/src/widgets/drag_scroll_widget.dart
@@ -4,14 +4,19 @@ import 'package:flutter/material.dart';
 // wrap this around a scroll view to enable mouse driven scrolling like
 // touch ui.  this is disabled by default
 class DragScrollWidget extends StatelessWidget {
-  const DragScrollWidget({required this.child});
+  const DragScrollWidget({
+    required this.child,
+    this.enable = true,
+  });
 
   final Widget child;
+  final bool enable;
 
   @override
   Widget build(BuildContext context) {
     return ScrollConfiguration(
-      behavior: MyCustomScrollBehavior(),
+      behavior:
+          enable ? MyCustomScrollBehavior() : const MaterialScrollBehavior(),
       child: child,
     );
   }
