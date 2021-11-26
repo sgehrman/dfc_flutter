9dd647ea24a2b9cc06d4e9830f0b626ab22b18a5
Steve Gehrman <sgehrman@gmail.com>
2021-11-25 21:49:09 -0800

    fix

--- lib/src/widgets/shared_snack_bar.dart
+++ lib/src/widgets/shared_snack_bar.dart
@@ -165,7 +165,6 @@ class _SharedSnackBarState extends State<SharedSnackBar>
       top: widget.onTop ? topPosition : null,
       bottom: !widget.onTop ? topPosition : null,
       left: 16,
-      right: 16,
       child: SlideTransition(
         position: offsetAnimation as Animation<Offset>,
         child: SafeArea(
@@ -206,11 +205,10 @@ class _StandardSnackBarState extends State<_StandardSnackBar> {
     return Material(
       type: MaterialType.transparency,
       child: Container(
-        constraints: const BoxConstraints(maxWidth: 1000),
         decoration: BoxDecoration(
           color: widget.error
               ? Colors.red[800]!.withOpacity(.9)
-              : Colors.orange[800]!.withOpacity(.9),
+              : Colors.green[800]!.withOpacity(.9),
           borderRadius: const BorderRadius.all(Radius.circular(12)),
           boxShadow: const [
             BoxShadow(
