b41e897ed681e5ce34d42b5c823d65921d7925eb
Steve Gehrman <sgehrman@gmail.com>
2021-12-23 17:37:14 -0800

    fix

--- lib/src/widgets/help_tip.dart
+++ lib/src/widgets/help_tip.dart
@@ -141,7 +141,8 @@ class _HelpTipContentState extends State<HelpTipContent> {
         imageBuilder: (uri, title, alt) {
           return const SizedBox();
         },
-        // softLineBreak: true,
+        softLineBreak: true,
+        fitContent: false,
         styleSheet: markdownStyleSheet,
         data: markdown,
       );
