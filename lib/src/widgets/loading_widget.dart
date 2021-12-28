62cadf5bdf33b71d603df383af593a7296aa2ef4
Steve Gehrman <sgehrman@gmail.com>
2021-12-27 19:46:12 -0800

    fix

--- lib/src/widgets/loading_widget.dart
+++ lib/src/widgets/loading_widget.dart
@@ -3,11 +3,14 @@ import 'package:flutter/material.dart';
 class LoadingWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
       child: SizedBox(
         height: 64,
         width: 64,
-        child: CircularProgressIndicator(),
+        child: CircularProgressIndicator(
+          color: Theme.of(context).primaryColor,
+          strokeWidth: 6,
+        ),
       ),
     );
   }
