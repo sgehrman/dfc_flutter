ab8713b56e032f4544c29af9385ff23376c49911
Steve Gehrman <sgehrman@gmail.com>
2022-01-13 03:38:38 -0800

    fix

--- lib/src/widgets/loading_widget.dart
+++ lib/src/widgets/loading_widget.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class LoadingWidget extends StatelessWidget {
+  const LoadingWidget({this.size = 140});
+
+  final double size;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: LoadingAnimationWidget.fourRotatingDots(
         color: Theme.of(context).primaryColor,
-        size: 64,
+        size: size,
       ),
 
       // child: SizedBox(
