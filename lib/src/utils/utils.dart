92ed8c3240ec2f1baad2e2cce1100a82c7c1546f
Steve Gehrman <sgehrman@gmail.com>
2022-03-13 23:25:17 -0700

    fix

--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -12,7 +12,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_parsed_text/flutter_parsed_text.dart';
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:path/path.dart' as p;
 import 'package:url_launcher/url_launcher.dart';
 
