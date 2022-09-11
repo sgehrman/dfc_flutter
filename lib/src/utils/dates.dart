8c0d98e05871e0a36c43b8f7be5478da27313784
Steve Gehrman <sgehrman@gmail.com>
2022-09-10 22:44:49 -0700

    fix

--- lib/src/utils/dates.dart
+++ lib/src/utils/dates.dart
@@ -41,7 +41,7 @@ class Dates {
   // 2022-10-31
   static String formatShortDate(DateTime? date) {
     if (date != null) {
-      final formatter = DateFormat('y-M-d');
+      final formatter = DateFormat('y-MM-d');
 
       return formatter.format(date);
     }
