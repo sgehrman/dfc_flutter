46de811ea4feaeb73aed7e6916b5856112d320d7
Steve Gehrman <sgehrman@gmail.com>
2023-01-02 03:39:10 +0900

    fix

--- lib/dfc_flutter_web.dart
+++ lib/dfc_flutter_web.dart
@@ -117,6 +117,7 @@ export 'src/widgets/theme_button.dart';
 export 'src/widgets/thumb_widget.dart';
 export 'src/widgets/thumbs_up_control.dart';
 export 'src/widgets/title_detail_row.dart';
+export 'src/widgets/tooltip_utils.dart';
 export 'src/widgets/transparent_appbar_theme.dart';
 export 'src/widgets/transparent_route.dart';
 export 'src/widgets/triangle_painter.dart';
