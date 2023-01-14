f72f722336e7703ea80f97d7eb733439807b2431
Steve Gehrman <sgehrman@gmail.com>
2023-01-13 20:13:29 -0800

    fix

--- lib/src/google_fonts/google_font_library.dart
+++ lib/src/google_fonts/google_font_library.dart
@@ -5,6 +5,10 @@ TextTheme themeWithGoogleFont(String fontName, TextTheme theme) {
   return GoogleFonts.getTextTheme(fontName, theme);
 }
 
+TextStyle styleWithGoogleFont(String fontName, TextStyle textStyle) {
+  return GoogleFonts.getFont(fontName, textStyle: textStyle);
+}
+
 List<String> googleFonts() {
   return GoogleFonts.asMap().keys.toList();
 }
