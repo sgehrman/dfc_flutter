908ba366cf4e0345e836d8240e1783f9720670b1
Steve Gehrman <sgehrman@gmail.com>
2023-05-21 05:25:59 -0700

    sdf

--- lib/src/widgets/fading_divider.dart
+++ lib/src/widgets/fading_divider.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 
 class FadingDivider extends StatelessWidget {
   const FadingDivider({
-    this.height = 1,
+    this.height = 0,
+    this.thickness = 1,
     this.color,
   });
 
   final double height;
+  final double thickness;
   final Color? color;
 
   @override
@@ -20,7 +22,7 @@ class FadingDivider extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.symmetric(vertical: height == 0 ? 0 : height / 2),
       child: SizedBox(
-        height: height,
+        height: thickness,
         child: DecoratedBox(
           decoration: BoxDecoration(
             gradient: LinearGradient(
