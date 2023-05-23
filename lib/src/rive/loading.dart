8d46b0d4467c6ce05581a1c2d166365d725d8ee7
Steve Gehrman <sgehrman@gmail.com>
2023-05-23 02:25:13 -0700

    sdf

--- lib/src/rive/loading.dart
+++ lib/src/rive/loading.dart
@@ -1,32 +1,32 @@
-import 'package:flutter/material.dart';
-import 'package:rive/rive.dart';
+// import 'package:flutter/material.dart';
+// import 'package:rive/rive.dart';
 
-class LoadingAnimation extends StatefulWidget {
-  @override
-  State<LoadingAnimation> createState() => _LoadingAnimationState();
-}
+// class LoadingAnimation extends StatefulWidget {
+//   @override
+//   State<LoadingAnimation> createState() => _LoadingAnimationState();
+// }
 
-class _LoadingAnimationState extends State<LoadingAnimation> {
-  late RiveAnimationController<dynamic> _controller;
+// class _LoadingAnimationState extends State<LoadingAnimation> {
+//   late RiveAnimationController<dynamic> _controller;
 
-  @override
-  void initState() {
-    super.initState();
-    _controller = SimpleAnimation('SlideThem');
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     _controller = SimpleAnimation('SlideThem');
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 100,
-      width: 100,
-      child: RiveAnimation.asset(
-        'assets/animations/loading.flr',
-        // color: Theme.of(context).primaryColor,
-        controllers: [_controller],
-        // Update the play state when the widget's initialized
-        // onInit: (_) => setState(() { _isPlaying = false}),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return SizedBox(
+//       height: 100,
+//       width: 100,
+//       child: RiveAnimation.asset(
+//         'assets/animations/loading.flr',
+//         // color: Theme.of(context).primaryColor,
+//         controllers: [_controller],
+//         // Update the play state when the widget's initialized
+//         // onInit: (_) => setState(() { _isPlaying = false}),
+//       ),
+//     );
+//   }
+// }
