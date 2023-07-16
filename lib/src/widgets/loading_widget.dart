d0aa0a06ce0d9c7b6872f1fd87a22bcd65ad36ab
Steve Gehrman <sgehrman@gmail.com>
2023-07-15 17:17:31 -0700

    sdf

--- lib/src/widgets/loading_widget.dart
+++ lib/src/widgets/loading_widget.dart
@@ -1,68 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 
-class LoadingWidget extends StatefulWidget {
+class LoadingWidget extends StatelessWidget {
   const LoadingWidget({
     required this.color,
+    this.delay = const Duration(milliseconds: 1000),
     this.size = 64,
   });
 
   final Color color;
   final double size;
-
-  @override
-  State<LoadingWidget> createState() => _LoadingWidgetState();
-}
-
-class _LoadingWidgetState extends State<LoadingWidget>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<Color?> _colorTween;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 3),
-    );
-
-    // is there a better way to this?
-    final customTween = Animatable<double>.fromCallback((double value) {
-      if (value <= 0.5) {
-        return 0.0001; // returning 0 didn't work?
-      }
-
-      return (value - 0.5) * 2;
-    });
-
-    final animation = _controller.drive<double>(customTween);
-
-    // fades in so it's not jarring
-    _colorTween = animation.drive<Color?>(
-      ColorTween(end: widget.color),
-    );
-
-    _controller.forward();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+  final Duration delay;
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: SizedBox(
-        height: widget.size,
-        width: widget.size,
+        height: size,
+        width: size,
         child: CircularProgressIndicator(
-          valueColor: _colorTween,
-          strokeWidth: widget.size * 0.1,
-        ),
+          color: color,
+          strokeWidth: size * 0.1,
+        )
+            .animate()
+            .effect(
+              curve: Curves.easeIn,
+              delay: delay,
+              duration: const Duration(milliseconds: 300),
+            )
+            .fadeIn(begin: 0),
       ),
     );
   }
