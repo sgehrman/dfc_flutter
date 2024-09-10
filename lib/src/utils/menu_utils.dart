61f4f973d300f64e22c25d30520c3817f9bc138d
Steve Gehrman <sgehrman@gmail.com>
2024-09-10 23:18:56 +0900

    sdf

--- lib/src/utils/menu_utils.dart
+++ lib/src/utils/menu_utils.dart
@@ -1,47 +1,5 @@
 import 'package:flutter/material.dart';
 
-class MenuUtils {
-  static Future<T?> displayMenu<T>({
-    required BuildContext context,
-    required Offset localPosition,
-    required List<PopupMenuEntry<T>> menuItems,
-  }) async {
-    final PopupMenuThemeData popupMenuTheme = PopupMenuTheme.of(context);
-    final RenderBox? button = context.findRenderObject() as RenderBox?;
-    final RenderBox? overlay =
-        Navigator.of(context).overlay!.context.findRenderObject() as RenderBox?;
-
-    if (button != null && overlay != null) {
-      final buttonRect = Rect.fromPoints(
-        button.localToGlobal(
-          localPosition,
-          ancestor: overlay,
-        ),
-        button.localToGlobal(
-          localPosition + const Offset(10, 10),
-          ancestor: overlay,
-        ),
-      );
-
-      final RelativeRect position = RelativeRect.fromRect(
-        buttonRect,
-        Offset.zero & overlay.size,
-      );
-
-      return showMenu<T>(
-        context: context,
-        elevation: popupMenuTheme.elevation,
-        items: menuItems,
-        position: position,
-        shape: popupMenuTheme.shape,
-        color: popupMenuTheme.color,
-      );
-    }
-
-    return null;
-  }
-}
-
 // use this so all our menus are the same heights (default is too big)
 PopupMenuEntry<T> popupMenuItem<T>({
   required Widget? child, // null for PopupMenuDivider
