f3f6feaca6ea7fc7bee7fb551542b95346ccffa9
Steve Gehrman <sgehrman@gmail.com>
2024-09-10 20:22:12 +0900

    sdf

--- lib/src/extensions/build_context_ext.dart
+++ lib/src/extensions/build_context_ext.dart
@@ -1,34 +1,36 @@
 import 'package:flutter/material.dart';
 
 extension BuildContextUtils on BuildContext {
-  Color primary() => Theme.of(this).colorScheme.primary;
-  Color onPrimary() => Theme.of(this).colorScheme.onPrimary;
-  Color onPrimaryFixedVariant() =>
+  Color get primary => Theme.of(this).colorScheme.primary;
+  Color get onPrimary => Theme.of(this).colorScheme.onPrimary;
+  Color get onPrimaryFixedVariant =>
       Theme.of(this).colorScheme.onPrimaryFixedVariant;
-  Color inversePrimary() => Theme.of(this).colorScheme.inversePrimary;
-  Color inverseSurface() => Theme.of(this).colorScheme.inverseSurface;
-  Color onInverseSurface() => Theme.of(this).colorScheme.onInverseSurface;
-  Color surface() => Theme.of(this).colorScheme.surface;
-  Color surfaceContainerHigh() =>
+  Color get inversePrimary => Theme.of(this).colorScheme.inversePrimary;
+  Color get inverseSurface => Theme.of(this).colorScheme.inverseSurface;
+  Color get onInverseSurface => Theme.of(this).colorScheme.onInverseSurface;
+  Color get surface => Theme.of(this).colorScheme.surface;
+  Color get surfaceContainerHigh =>
       Theme.of(this).colorScheme.surfaceContainerHigh;
-  Color surfaceContainerHighest() =>
+  Color get surfaceContainerHighest =>
       Theme.of(this).colorScheme.surfaceContainerHighest;
-  Color surfaceContainerLowest() =>
+  Color get surfaceContainerLowest =>
       Theme.of(this).colorScheme.surfaceContainerLowest;
-  Color surfaceBright() => Theme.of(this).colorScheme.surfaceBright;
-  Color onSurface() => Theme.of(this).colorScheme.onSurface;
-  Color onSurfaceVariant() => Theme.of(this).colorScheme.onSurfaceVariant;
-  Color surfaceContainer() => Theme.of(this).colorScheme.surfaceContainer;
-  Color surfaceContainerLow() => Theme.of(this).colorScheme.surfaceContainerLow;
-  Color lightPrimary(BuildContext context, {double opacity = 0.4}) =>
+  Color get surfaceBright => Theme.of(this).colorScheme.surfaceBright;
+  Color get onSurface => Theme.of(this).colorScheme.onSurface;
+  Color get onSurfaceVariant => Theme.of(this).colorScheme.onSurfaceVariant;
+  Color get surfaceContainer => Theme.of(this).colorScheme.surfaceContainer;
+  Color get surfaceContainerLow =>
+      Theme.of(this).colorScheme.surfaceContainerLow;
+  Color lightPrimary({double opacity = 0.4}) =>
       Theme.of(this).colorScheme.primary.withOpacity(opacity);
-  Color tertiary() => Theme.of(this).colorScheme.tertiary;
-  Color tertiaryContainer() => Theme.of(this).colorScheme.tertiaryContainer;
-  Color onTertiary() => Theme.of(this).colorScheme.onTertiary;
-  Color onTertiaryContainer() => Theme.of(this).colorScheme.onTertiaryContainer;
-  Color secondary() => Theme.of(this).colorScheme.secondary;
-  Color secondaryContainer() => Theme.of(this).colorScheme.secondaryContainer;
-  Color onSecondary() => Theme.of(this).colorScheme.onSecondary;
-  Color onSecondaryContainer() =>
+  Color get tertiary => Theme.of(this).colorScheme.tertiary;
+  Color get tertiaryContainer => Theme.of(this).colorScheme.tertiaryContainer;
+  Color get onTertiary => Theme.of(this).colorScheme.onTertiary;
+  Color get onTertiaryContainer =>
+      Theme.of(this).colorScheme.onTertiaryContainer;
+  Color get secondary => Theme.of(this).colorScheme.secondary;
+  Color get secondaryContainer => Theme.of(this).colorScheme.secondaryContainer;
+  Color get onSecondary => Theme.of(this).colorScheme.onSecondary;
+  Color get onSecondaryContainer =>
       Theme.of(this).colorScheme.onSecondaryContainer;
 }
