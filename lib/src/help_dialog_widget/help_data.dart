ebccde75686c728bd5f77d704a9bbdf4fd4d26d8
Steve Gehrman <sgehrman@gmail.com>
2024-09-26 20:38:54 +0900

    sdf

--- lib/src/help_dialog_widget/help_data.dart
+++ lib/src/help_dialog_widget/help_data.dart
@@ -8,4 +8,41 @@ class HelpData {
 
   final ParagrafSpec title;
   final ParagrafSpec message;
+
+  // --------------------------------------
+  // static helpers
+
+  static void _writeItem(ParagrafSpec item, StringBuffer buffer) {
+    buffer.write(item.text);
+    for (final c in item.children) {
+      _writeItem(c, buffer);
+    }
+  }
+
+  static String toHtml(List<HelpData> data) {
+    final buffer = StringBuffer();
+
+    buffer.write('<!DOCTYPE html><html><head>');
+    buffer.write('<meta charSet="utf-8"/>');
+    buffer.write('<title>Deckr Help</title>');
+    buffer.write(
+      '<meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"/>',
+    );
+    buffer.write('</head><body style="max-width: 600px">');
+
+    for (final item in data) {
+      buffer.write('<h4>');
+      _writeItem(item.title, buffer);
+      buffer.write('</h4>');
+
+      buffer.write('<p>');
+      _writeItem(item.message, buffer);
+      buffer.write('</p>');
+    }
+
+    buffer.write('</body>');
+    buffer.write('</html>');
+
+    return buffer.toString();
+  }
 }
