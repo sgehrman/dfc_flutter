fdaaf0ba16a8e512198b8ceedbf8e3ee33153d42
Steve Gehrman <sgehrman@gmail.com>
2024-10-25 19:55:22 -0700

    sdf

--- lib/l10n/app_locs.dart
+++ lib/l10n/app_locs.dart
@@ -1,3 +1,4 @@
 import 'package:dfc_flutter/l10n/app_localizations.dart';
 
 const dfcFlutterLocDelegates = AppLocalizations.localizationsDelegates;
+const dfcFlutterLocales = AppLocalizations.supportedLocales;
