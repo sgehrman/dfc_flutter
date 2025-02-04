231a2df89f3ba27eb6a7b7d6924c9bcd899349a3
Steve Gehrman <sgehrman@gmail.com>
2025-02-03 18:34:41 -0800

    sdf

--- lib/src/widgets/df_tooltip.dart
+++ lib/src/widgets/df_tooltip.dart
@@ -2,28 +2,31 @@ import 'package:dfc_flutter/src/utils/utils.dart';
 import 'package:flutter/material.dart';
 
 // same as Tooltip, but handles null messages without exceptions
+// it also wraps text so the tooltip isn't so wide
 
 class DFTooltip extends StatelessWidget {
   const DFTooltip({
     required this.message,
     required this.child,
+    this.preferBelow,
   });
 
   final String? message;
   final Widget child;
+  final bool? preferBelow;
 
   String _wrapString(String message) {
     final words = message.split(' ');
 
     final buffer = StringBuffer();
 
-    int cnt = 0;
+    int letterCnt = 0;
     for (final word in words) {
-      cnt += word.length;
+      letterCnt += word.length;
       buffer.write('$word ');
 
-      if (cnt > 40) {
-        cnt = 0;
+      if (letterCnt > 40) {
+        letterCnt = 0;
         buffer.write('\n');
       }
     }
@@ -36,6 +39,7 @@ class DFTooltip extends StatelessWidget {
     if (Utils.isNotEmpty(message)) {
       return Tooltip(
         message: _wrapString(message!),
+        preferBelow: preferBelow,
         child: child,
       );
     }
