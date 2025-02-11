3af09c83fdfe6db4797be847764da17b7de795b2
Steve Gehrman <sgehrman@gmail.com>
2025-02-10 16:41:07 -0800

    sdf

--- lib/src/widgets/linkify_text/regex.dart
+++ lib/src/widgets/linkify_text/regex.dart
@@ -5,7 +5,7 @@
 import 'package:dfc_flutter/src/widgets/linkify_text/linkify.dart';
 
 String urlRegExp =
-    r'(((http|https)://)?(www\.)?[a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*))';
+    r'(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})';
 
 String hashtagRegExp =
     r'#[a-zA-Z\u00C0-\u01B4\w_\u1EA0-\u1EF9!$%^&]{1,}(?=\s|$)';
