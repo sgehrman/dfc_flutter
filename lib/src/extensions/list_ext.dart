3dcd44a69c582a184a4d03c1464440ab87b3a6db
Steve Gehrman <sgehrman@gmail.com>
2025-02-24 02:48:36 -0800

    sdf

--- lib/src/extensions/list_ext.dart
+++ lib/src/extensions/list_ext.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
-extension ExtendedList on List<dynamic> {
+extension ExtendedList on List<Widget> {
   List<Widget> addDividers({Widget divider = const Divider(height: 6)}) {
     final List<Widget> result = [];
 
     if (length < 2) {
-      return this as List<Widget>;
+      return this;
     }
 
     for (int i = 0; i < length; i++) {
-      result.add(this[i] as Widget);
+      result.add(this[i]);
 
       if (i < length - 1) {
         result.add(divider);
@@ -18,12 +18,14 @@ extension ExtendedList on List<dynamic> {
 
     return result;
   }
+}
 
-  List<T> truncate<T>(int limit) {
+extension GenericList<T> on List<T> {
+  List<T> truncate(int limit) {
     if (length > limit) {
-      return sublist(0, limit) as List<T>;
+      return sublist(0, limit);
     }
 
-    return this as List<T>;
+    return this;
   }
 }
