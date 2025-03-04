b82429f15360e354a0c62cd4d924eb6b25200009
Steve Gehrman <sgehrman@gmail.com>
2025-03-04 14:37:50 -0800

    sdf

--- lib/src/menu_button_bar/menu_button_bar_utils.dart
+++ lib/src/menu_button_bar/menu_button_bar_utils.dart
@@ -51,7 +51,7 @@ class MenuButtonBarUtils {
       ),
       shape: const WidgetStatePropertyAll(
         RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(6)),
+          borderRadius: BorderRadius.all(Radius.circular(4)),
         ),
       ),
       visualDensity: VisualDensity.compact,
