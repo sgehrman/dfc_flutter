ed02942380abb803aebf577b30d9154b826a688f
Steve Gehrman <sgehrman@gmail.com>
2025-04-30 00:57:23 +0900

    sdf

--- lib/src/extensions/build_context_ext.dart
+++ lib/src/extensions/build_context_ext.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 extension BuildContextUtils on BuildContext {
   Color get primary => Theme.of(this).colorScheme.primary;
   Color get onPrimary => Theme.of(this).colorScheme.onPrimary;
+  Color get onPrimaryFixed => Theme.of(this).colorScheme.onPrimaryFixed;
   Color get onPrimaryFixedVariant =>
       Theme.of(this).colorScheme.onPrimaryFixedVariant;
   Color get inversePrimary => Theme.of(this).colorScheme.inversePrimary;
