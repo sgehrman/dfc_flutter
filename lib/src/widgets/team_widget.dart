25fb05cd15cc6081593c8297d5b59ff5fec7dc87
Steve Gehrman <sgehrman@gmail.com>
2025-07-06 16:06:34 -0700

    sdf

--- lib/src/widgets/team_widget.dart
+++ lib/src/widgets/team_widget.dart
@@ -1,57 +1,67 @@
-import 'package:dfc_flutter/src/widgets/drag_scroll_widget.dart';
+import 'package:dfc_flutter/dfc_flutter_web_lite.dart';
 import 'package:flutter/material.dart';
 
 class Employee {
   const Employee({
     required this.name,
+    required this.position,
     required this.imagePath,
     required this.biography,
   });
   final String name;
+  final String position;
   final String imagePath;
   final String biography;
 }
 
-// https://docs.google.com/document/d/1VMcpASB9O4Wxq2AppZ1TS-uR-k_9VMCcWRbmcxPDoB4/edit?tab=t.0
+// =======================================================
 
 class TeamWidget extends StatefulWidget {
   const TeamWidget({super.key});
+
+  // https://docs.google.com/document/d/1VMcpASB9O4Wxq2AppZ1TS-uR-k_9VMCcWRbmcxPDoB4/edit?tab=t.0
   static const List<Employee> employees = [
     Employee(
       name: 'Alexandra',
       imagePath: 'assets/team_images/alexandra.jpeg',
+      position: 'Head of Operations',
       biography:
-          'Head of Operations Alexandra has been with Cocoatech since 2005, wearing many hats over the years before officially taking the reins as Head of Operations. She keeps the wheels turning behind the scenes — from managing payment systems and customer support to making sure things just work (and work well). She\'s passionate about clear communication, good user experience, and bringing structure to chaos. utside of Cocoatech, Alexandra lives in Germany with her husband and two children. When she’s not coordinating teams across time zones, you’ll likely find her lacing up for a figure skating session, switching between the five languages that she speaks, or digging into a new technical system just for fun.',
-    ),
-    Employee(
-      name: 'Arman',
-      imagePath: 'assets/team_images/arman.jpeg',
-      biography:
-          'Community Manager Arman is a tech enthusiast with a passion for innovation, always on top of the latest trends and tools to provide efficient and effective support. Whether you\'re facing technical issues or just need some guidance in Discord, Arman has your back. When not in school earning his college degrees, he enjoys roaming around Armenia, where he calls home.',
-    ),
-    Employee(
-      name: 'Dragan',
-      imagePath: 'assets/team_images/dragan.jpeg',
-      biography:
-          'CTO  Dragan got his first computing experience in 1983 on Sinclair ZX Spectrum and wrote his first code for it in BASIC a year later. He has been a Mac user since 1994, a Mac coder for fun since 2002, and has been doing it professionally since 2009. In his spare time, he loves to ride his bicycle through the Netherlands and makes fighter plane models for fun.',
-    ),
-    Employee(
-      name: 'Naum',
-      imagePath: 'assets/team_images/naum.jpeg',
-      biography:
-          'Customer Support Consultant Naum has a love of for problem-solving and a drive to help others. He thrives in fast-paced environments and is dedicated to delivering quality service and ensuring clients have the best experience possible. Naum is an avid traveler and calls all of Europe home.',
+          'Alexandra has been with Cocoatech since 2005, wearing many hats over the years before officially taking the reins as Head of Operations. She keeps the wheels turning behind the scenes — from managing payment systems and customer support to making sure things just work (and work well). She\'s passionate about clear communication, good user experience, and bringing structure to chaos. utside of Cocoatech, Alexandra lives in Germany with her husband and two children. When she’s not coordinating teams across time zones, you’ll likely find her lacing up for a figure skating session, switching between the five languages that she speaks, or digging into a new technical system just for fun.',
     ),
     Employee(
       name: 'Kim',
       imagePath: 'assets/team_images/kim.jpeg',
+      position: 'Marketing Manager',
       biography:
-          'Marketing Manager Kim is a native Californian, but she began traveling three years ago and now calls Mexico home. Raised up in the SF tech scene for over 15 years, she shifted to her marketing work from Tech to Psychedelics and Mental Health, an interest Steve and Kim share! She has been with Cocoatech for over five years alongside her other clients. When not behind her computer, you will find her walking on the beach or hopping in an ice bath under the Mexican sun.',
+          'Kim is a native Californian, but she began traveling three years ago and now calls Mexico home. Raised up in the SF tech scene for over 15 years, she shifted to her marketing work from Tech to Psychedelics and Mental Health, an interest Steve and Kim share! She has been with Cocoatech for over five years alongside her other clients. When not behind her computer, you will find her walking on the beach or hopping in an ice bath under the Mexican sun.',
+    ),
+    Employee(
+      name: 'Arman',
+      imagePath: 'assets/team_images/arman.jpeg',
+      position: 'Community Manager',
+      biography:
+          'Arman is a tech enthusiast with a passion for innovation, always on top of the latest trends and tools to provide efficient and effective support. Whether you\'re facing technical issues or just need some guidance in Discord, Arman has your back. When not in school earning his college degrees, he enjoys roaming around Armenia, where he calls home.',
+    ),
+    Employee(
+      name: 'Dragan',
+      imagePath: 'assets/team_images/dragan.jpeg',
+      position: 'CTO',
+      biography:
+          'Dragan got his first computing experience in 1983 on Sinclair ZX Spectrum and wrote his first code for it in BASIC a year later. He has been a Mac user since 1994, a Mac coder for fun since 2002, and has been doing it professionally since 2009. In his spare time, he loves to ride his bicycle through the Netherlands and makes fighter plane models for fun.',
+    ),
+    Employee(
+      name: 'Naum',
+      imagePath: 'assets/team_images/naum.jpeg',
+      position: 'Customer Support Consultant',
+      biography:
+          'Naum has a love of for problem-solving and a drive to help others. He thrives in fast-paced environments and is dedicated to delivering quality service and ensuring clients have the best experience possible. Naum is an avid traveler and calls all of Europe home.',
     ),
     Employee(
       name: 'Steve',
       imagePath: 'assets/team_images/steve.jpeg',
+      position: 'CEO',
       biography:
-          'CEO Steve has been coding since 1986. He has written and designed two popular macOS apps: Final Draft and Path Finder and is working on a third. A Los Angeles Native, he currently lives in San Francisco, where Cocoatech has its headquarters (in his home office!). His wife and two children travel frequently to Japan, where his daughter is a rising music star. When he is not rollerskating in Golden Gate Park or spending quality time with his family, he is working on Deckr, Cocoatech\'s newest app.',
+          'Steve has been coding since 1986. He has written and designed two popular macOS apps: Final Draft and Path Finder and is working on a third. A Los Angeles Native, he currently lives in San Francisco, where Cocoatech has its headquarters (in his home office!). His wife and two children travel frequently to Japan, where his daughter is a rising music star. When he is not rollerskating in Golden Gate Park or spending quality time with his family, he is working on Deckr, Cocoatech\'s newest app.',
     ),
   ];
 
@@ -390,7 +400,20 @@ class _EmployeeCardState extends State<EmployeeCard>
                                     .titleLarge
                                     ?.copyWith(
                                       fontWeight: FontWeight.bold,
-                                      color: Colors.grey.shade800,
+                                      color: context.textColor,
+                                    ),
+                                textAlign: TextAlign.center,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              const SizedBox(height: 4),
+                              Text(
+                                widget.employee.position,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleMedium
+                                    ?.copyWith(
+                                      color: context.lightTextColor,
                                     ),
                                 textAlign: TextAlign.center,
                                 maxLines: 1,
