882d8a7f873b68fd47b0692569005863390e2a69
Steve Gehrman <sgehrman@gmail.com>
2025-10-22 16:42:22 -0700

    dfg

--- lib/src/seo/seo_app.dart
+++ lib/src/seo/seo_app.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:seo/seo.dart';
 
 class SeoApp extends StatelessWidget {
-  const SeoApp({required this.child});
+  const SeoApp({required this.child, required this.enabled});
 
   final Widget child;
+  final bool enabled;
 
   @override
   Widget build(BuildContext context) {
-    return SeoController(tree: WidgetTree(context: context), child: child);
+    return SeoController(
+        tree: WidgetTree(context: context), enabled: enabled, child: child);
   }
 }
